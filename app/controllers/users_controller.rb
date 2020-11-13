f2dff3e9191166d828f540296f64a4989cc1e307
kevin Cotrina <kcotrinam92@gmail.com>
2020-11-13 18:57:08 -0500

    Add html form and create action controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
     
     def create
-       
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        if @user.save
+          flash[:success] = "User successfully created"
+          redirect_to new_user_path
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
     end
-    
 end
